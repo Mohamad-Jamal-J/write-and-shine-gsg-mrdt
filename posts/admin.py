09721784ea2b710024a71db6049a19276bd9da84
Sarah Abu irmeileh <211027@ppu.edu.ps>
2024-09-27 08:47:04 +0300

    Register the tables in Django admin panel

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from posts.models import Comment, Like, Post, Post_tag, Tag
+
+admin.site.register(Post)
+admin.site.register(Tag)
+admin.site.register(Post_tag)
+admin.site.register(Comment)
+admin.site.register(Like)
