42f35c7f17c629712cfdc256d1f2c9a2bffeb460
Sarah Abu irmeileh <211027@ppu.edu.ps>
2024-09-29 15:55:10 +0300

    Edit search_post to search posts according to title, body and tags, and remove the tags search bar from front

--- search/views.py
+++ search/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from posts.models import Post, Tag
+from django.db.models import Q
 
 # Create your views here.
 def index(request):
@@ -20,8 +21,27 @@ def update_post_metadata(posts):
 
 @api_view(['GET'])
 def search_post(request):
-    query_post_name = request.GET.get('post_name', '')
-    posts = Post.objects.filter(title__icontains=query_post_name) | Post.objects.filter(body__icontains=query_post_name)
+    query_post_name = request.GET.get('post_name', '').strip()
+    
+    # Search for posts by title or body using Q objects
+    posts_by_title_or_body = Post.objects.filter(
+        Q(title__icontains=query_post_name) | Q(body__icontains=query_post_name)
+    )
+    
+    # Capitalize the first letter of the tag query and filter by tags
+    query_tag_name = query_post_name.capitalize()
+    tag = Tag.objects.filter(name=query_tag_name).first()
+
+    if tag:
+        posts_by_tags = Post.objects.filter(tags=tag)
+    else:
+        posts_by_tags = Post.objects.none()
+
+    # Combine the two queries using Q objects
+    posts = Post.objects.filter(
+        Q(id__in=posts_by_title_or_body.values_list('id', flat=True)) | 
+        Q(id__in=posts_by_tags.values_list('id', flat=True))
+    ).distinct()
 
     # Update post metadata
     posts = update_post_metadata(posts)
