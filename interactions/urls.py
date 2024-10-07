c566873738fac2205819001095ecfc6d03ab5c20
Sarah Abu irmeileh <211027@ppu.edu.ps>
2024-10-07 08:58:10 +0300

    Create API to check if the user who is logged in has like specific post, to help Front-End to display a color for a like (If a user likes a post, the like icon will be displayed in a different color than if the user doesn't like the post)

--- interactions/urls.py
+++ interactions/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('<int:post_id>/comment/', views.comment_post, name='comment_post'),
     path('comments/<int:comment_id>/edit/', views.edit_comment, name='edit_comment'),
     path('comments/<int:comment_id>/delete/', views.delete_comment, name='delete_comment'),
+    path('<int:post_id>/has-liked/', views.has_user_liked_post, name='has_user_liked_post'),
+
 ]
